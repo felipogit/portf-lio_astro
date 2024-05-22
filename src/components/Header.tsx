e0e9a7605a332005b20bef7a19be673edd53cfc3
felipogit <felipeesped0023@gmail.com>
2024-05-22 10:47:36 -0300

    dark mode localstorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,17 @@ export const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
+        const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+        setDarkMode(savedDarkMode);
+    }, []);
+
+    useEffect(() => {
         if (darkMode) {
             document.documentElement.classList.add('dark');
         } else {
             document.documentElement.classList.remove('dark');
         }
+        localStorage.setItem('darkMode', darkMode.toString());
     }, [darkMode]);
 
     return (
@@ -21,12 +27,12 @@ export const Header = () => {
                         {menuOpen ? <MdClose size={30} /> : <MdMenu size={30} />}
                     </button>
                     <ul className={`fixed top-20 left-0 w-full bg-pink-500 dark:bg-zinc-800 transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:static md:flex md:translate-x-0 md:bg-transparent md:dark:bg-transparent md:space-x-4 flex flex-col items-center justify-center md:flex-row md:items-center md:justify-start`}>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Home</li>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Sobre</li>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Habilidades</li>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Serviços</li>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Portfólio</li>
-                        <li className="text-xl cursor-pointer p-4 md:p-0 md:mx-auto">Contato</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600 ">Home</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Sobre</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Habilidades</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Serviços</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Portfólio</li>
+                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Contato</li>
                     </ul>
                 </div>
                 <button onClick={() => setDarkMode(!darkMode)} className="md:ml-4">
