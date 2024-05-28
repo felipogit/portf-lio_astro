c9027a1fe4530b4c72c213f1e14448d47c0ad142
felipogit <felipeesped0023@gmail.com>
2024-05-28 08:43:40 -0300

    ajustes para melhoria do código

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,76 @@
 import { useState, useEffect } from "react";
-import { MdOutlineDarkMode, MdOutlineLightMode, MdMenu, MdClose } from "react-icons/md";
+import {
+  MdOutlineDarkMode,
+  MdOutlineLightMode,
+  MdMenu,
+  MdClose,
+} from "react-icons/md";
+
+import clsx from "clsx";
+
+const Menus = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Sobre",
+    path: "/sobre",
+  },
+  {
+    label: "Projetos",
+    path: "/projetos",}
+];
 
 export const Header = () => {
-    const [darkMode, setDarkMode] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    Boolean(localStorage.getItem("darkMode"))
+  );
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    useEffect(() => {
-        const savedDarkMode = localStorage.getItem('darkMode') === 'true';
-        setDarkMode(savedDarkMode);
-    }, []);
+  const handleDarkModeSelectorClick = () => {
+    setDarkMode((prevState) => !prevState);
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    document.documentElement.classList.toggle("dark");
+  };
 
-    useEffect(() => {
-        if (darkMode) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-        localStorage.setItem('darkMode', darkMode.toString());
-    }, [darkMode]);
-
-    return (
-        <header className="w-full flex items-center justify-between fixed top-0 left-0 z-50 bg-pink-500 text-white dark:bg-zinc-800 dark:text-white h-20 px-4">
-            <div className="flex items-center justify-between w-full max-w-6xl mx-auto">
-                <div className="flex items-center">
-                    <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden">
-                        {menuOpen ? <MdClose size={30} /> : <MdMenu size={30} />}
-                    </button>
-                    <ul className={`fixed top-20 left-0 w-full bg-pink-500 dark:bg-zinc-800 transition-transform transform ${menuOpen ? 'translate-x-0' : '-translate-x-full'} md:static md:flex md:translate-x-0 md:bg-transparent md:dark:bg-transparent md:space-x-4 flex flex-col items-center justify-center md:flex-row md:items-center md:justify-start`}>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600 ">Home</li>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Sobre</li>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Habilidades</li>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Serviços</li>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Portfólio</li>
-                        <li className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600">Contato</li>
-                    </ul>
-                </div>
-                <button onClick={() => setDarkMode(!darkMode)} className="md:ml-4">
-                    {darkMode ? <MdOutlineLightMode size={30} /> : <MdOutlineDarkMode size={30} />}
-                </button>
-            </div>
-        </header>
-    );
+  return (
+    <header className="w-full flex items-center justify-between sticky top-0 left-0 z-50 bg-pink-500 text-white dark:bg-zinc-800 dark:text-white h-20 px-4">
+      <div className="flex items-center justify-between w-full max-w-6xl mx-auto">
+        <div className="flex items-center">
+          <button
+            onClick={() => setMenuOpen((prevState) => !prevState)}
+            className="md:hidden"
+          >
+            {menuOpen ? <MdClose size={30} /> : <MdMenu size={30} />}
+          </button>
+          <ul
+            className={clsx(
+              "fixed top-20 left-0 w-full bg-pink-500 dark:bg-zinc-800 transition-transform transform md:static md:flex md:translate-x-0 md:bg-transparent md:dark:bg-transparent md:space-x-4 flex flex-col items-center justify-center md:flex-row md:items-center md:justify-start",
+              menuOpen && "translate-x-0",
+              !menuOpen && "-translate-x-full"
+            )}
+          >
+            {Menus.map((menu) => (
+              <a href={menu.path}>
+                <li
+                  className="w-full text-xl cursor-pointer p-4 md:p-0 md:mx-auto hover:bg-pink-600"
+                  key={menu.label}
+                >
+                  {menu.label}
+                </li>
+              </a>
+            ))}
+          </ul>
+        </div>
+        <button onClick={handleDarkModeSelectorClick} className="md:ml-4">
+          {darkMode ? (
+            <MdOutlineLightMode size={30} />
+          ) : (
+            <MdOutlineDarkMode size={30} />
+          )}
+        </button>
+      </div>
+    </header>
+  );
 };
