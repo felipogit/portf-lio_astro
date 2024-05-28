295050050655f5b0f2151059f83220751c4e7dfd
felipogit <felipeesped0023@gmail.com>
2024-05-28 15:57:53 -0300

    reparo no modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,20 +4,19 @@ interface ModalProps {
   children: React.ReactNode;
   isOpen: boolean;
   setIsModalOpen: (isOpen: boolean) => void;
-  
 }
 
 export const Modal = ({ children, isOpen, setIsModalOpen }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex sm:items-center justify-center z-50 ">
-      <div className="bg-white dark:bg-zinc-800 p-8 rounded-lg shadow-lg  w-full max-w-[100rem] relative">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white dark:bg-zinc-800 p-8 rounded-lg shadow-lg w-full max-w-[100rem] relative overflow-auto max-h-full">
         <button
           onClick={() => setIsModalOpen(false)}
           className="absolute top-2 right-2 text-black dark:text-white"
         >
-            X
+          X
         </button>
         {children}
       </div>
